cedfa34471f1a95378f1a88d7f554bc9ab741fdc
syafi <syafimohamad13@gmail.com>
2022-09-06 19:14:09 +0800

    update calculation function to display in screen

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function subtract (a,b){return a-b};
 
 function multiply(a,b){return a*b};
 
-function divide (a,b){a/b};
+function divide (a,b){return Math.round((a/b)* 10) / 10 };
 
 function operate( operator , a, b){
 
@@ -19,7 +19,11 @@ function operate( operator , a, b){
             return  multiply(a,b);
             break;
         case '÷':
-            return divide(a,b);
+            console.log(a);
+            if(a == 0 || b == 0){
+                return "ERROR";
+            }else
+                return divide(a,b);
             break;
 
     }
@@ -49,34 +53,91 @@ function classifyArrays(expArray){
     return classifyArray;
 }
 
+function clear(){
+
+    let currentScreenText = document.querySelector(".screen-text");
+    let currentScreenResult = document.querySelector(".screen-result");
+
+    currentScreenText.textContent = "";
+    currentScreenResult.textContent = "";
+    currentOperator = "";
+    firstNum = 0;
+    secondNum = 0;
+}
+
 function updateScreen(e){
     let currentScreenText = document.querySelector(".screen-text");
     let currentScreenResult = document.querySelector(".screen-result");
     
     const key = e.target.getAttribute("data-key");
 
+    //clear the screen if "=" at the start of expression or when the expression is incomplete
+    if(key=="="){
+        if(firstNum==0 || (firstNum!=0 && currentOperator!="" && !currentScreenResult.textContent) ){
+            clear();
+            return;
+        }        
+    }
+
+    //clear the screen if operators are clicked again before numbers
+    if(operators.includes(key) && key != "="){
+        if(!currentScreenResult.textContent){
+            clear();
+            return;
+        }
+    }
+
     if(key=="clear"){
 
-        currentScreenText.textContent = "";
-        currentScreenResult.textContent = "";
+        clear();
 
     }else if(operators.includes(key)){
 
         if(firstNum == 0){
-
             firstNum = Number.parseInt(currentScreenResult.textContent);
             currentScreenText.textContent = currentScreenResult.textContent += key;
             currentOperator = key;
             currentScreenResult.textContent = "";
 
-        }else{
-           
+        }else if(key == "=" ){          
+
             secondNum = Number.parseInt(currentScreenResult.textContent);
+            console.log(currentOperator+" "+firstNum+" "+secondNum);
             result = operate(currentOperator,firstNum,secondNum);
-            currentScreenText.textContent = result += key;
+
+            if(result == "ERROR"){
+                alert(errorMessage);
+                clear();
+                return;
+            }
+
+            currentScreenText.textContent = result;
             firstNum = result;  
-            currentOperator = key;       
+            currentOperator = "";       
             currentScreenResult.textContent = "";
+
+        }else{
+
+            if(currentScreenResult.textContent){
+                secondNum = Number.parseInt(currentScreenResult.textContent);
+                result = operate(currentOperator,firstNum,secondNum);
+
+                if(result == "ERROR"){
+                    alert(errorMessage);
+                    clear();
+                    return;
+                }
+
+                currentScreenText.textContent = result + key;
+                firstNum = result;  
+                currentOperator = key;       
+                currentScreenResult.textContent = "";
+
+            }else{
+                currentScreenText.textContent = currentScreenText.textContent += key;
+                currentOperator = key;
+                currentScreenResult.textContent = "";
+            }
 
         }
 
@@ -91,6 +152,7 @@ const operators = ["+","-","x","÷","="];
 const keypads = document.querySelectorAll("li");
 let currentOperator = "";
 let firstNum = 0,secondNum = 0;
+let errorMessage = "You cant do that here dimwit";
 
 keypads.forEach(keypad=>keypad.addEventListener('click', updateScreen));
 
